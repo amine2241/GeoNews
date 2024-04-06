180064febd19b1e884f266f0c297ddc2f812bca4
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-04-06 23:11:52 +0000

    testing api request to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:9000/JSON/data')
+        .then(response => response.text())
+        .then(data => setMessage(data))
+        .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          hello
+        <h1>{message}</h1>
+      </div>
   );
 }
 
