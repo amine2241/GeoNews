bc34e6a257868a82df1988f2cdc87385d71b367a
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-04-18 14:37:03 +0100

    added react-leaflet api

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:9000/JSON/data')
-        .then(response => response.text())
-        .then(data => setMessage(data))
-        .catch(error => console.error('Error:', error));
-  }, []);
-
   return (
-      <div>
-          hello
-        <h1>{message}</h1>
-      </div>
+<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   );
 }
 
