3c4966be893161a5f229b14b94d89a418b6110c6
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-04-21 02:09:30 +0100

    added location click event

--- client/src/layout/Map.js
+++ client/src/layout/Map.js
@@ -1,18 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
 export default function Navbar(){
+    function MapEvents() {
+        const map = useMapEvents({
+           click() {
+             console.log("hello")
+             map.locate();
+           },
+           locationfound(e){
+            console.log(e.latlng);
+           }
+        });
+       
+        return null; // This component doesn't render anything itself
+       }
+    
     return(
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} maxZoom={11} >
+  <TileLayer
+    attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+    subdomains={'abcd'}
+    maxZoom = { 20}
+  />
+  <MapEvents/>
+
+</MapContainer>
+
     )
 }
