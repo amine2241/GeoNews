d40a195e1ff454cfa8ed6d43029591245fe65208
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-04-29 12:24:31 +0100

    signup interface

--- client/src/Components/SignUpForm.js
+++ client/src/Components/SignUpForm.js
@@ -1,25 +1,80 @@
 import React from 'react'
+import Sign from "../images/signup_image.png"
 
 const SignUpForm = () => {
   return (
-<div className="artboard phone-1 justify-center items-center">
-    <label className="input input-bordered flex items-center gap-2">
-   <input type="text" className="grow" placeholder="Search" />
-   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
- </label>
- <label className="input input-bordered flex items-center gap-2">
-   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-   <input type="text" className="grow" placeholder="Email" />
- </label>
- <label className="input input-bordered flex items-center gap-2">
-   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-   <input type="text" className="grow" placeholder="Username" />
- </label>
- <label className="input input-bordered flex items-center gap-2">
-   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-   <input type="password" className="grow" value="password" />
- </label>
-</div>
+    <div className="flex justify-center items-center w-full  min-h-screen bg-white px-5 py-5">
+    <div className="xl:max-w-7xl bg-white drop-shadow-xl border border-black/20 w-full rounded-md flex justify-between items-stretch px-5 xl:px-5 py-5">
+      <div className="sm:w-[60%] lg:w-[50%] bg-cover bg-center items-center justify-center hidden md:flex ">
+        {/* <img
+          src="./images/undra"
+          alt="login"
+          className="h-[500px]"
+        /> */}
+        <img src={Sign} className="h-[550px]"/>
+      </div>
+      <div className="mx-auto w-full lg:w-1/2 md:p-10 py-5 md:py-0">
+        <h1 className="text-center text-2xl sm:text-3xl font-semibold text-[#00BFA6]">
+          Create Account
+        </h1>
+        <div className="w-full mt-5 sm:mt-8">
+          <div className="mx-auto w-full sm:max-w-md md:max-w-lg flex flex-col gap-5">
+            <div className="flex flex-col sm:flex-row gap-3">
+              <input
+                type="text"
+                placeholder="Enter Your First Name"
+                className="input input-bordered border-[#00BFA6] w-full max-w-xs text-black placeholder:text-black/70"
+              />
+              <input
+                type="text"
+                placeholder="Enter Your Last Name"
+                className="input input-bordered border-[#00BFA6] w-full max-w-xs text-black placeholder:text-black/70"
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              className="input input-bordered border-[#00BFA6] w-full text-black placeholder:text-black/70"
+            />
+            <input
+              type="text"
+              placeholder="Enter Your Phone No"
+              className="input input-bordered border-[#00BFA6] w-full text-black placeholder:text-black/70"
+            />
+            <input
+              type="Password"
+              placeholder="Enter Your Password"
+              className="input input-bordered border-[#00BFA6] w-full text-black placeholder:text-black/70"
+            />
+            <div className="flex items-center gap-1.5  justify-start pl-2">
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <input
+                    type="checkbox"
+                    className="checkbox-xs checkbox-primary"
+                  />
+                </label>
+              </div>
+              <h3 className="flex items-center whitespace-nowrap text-xs text-black">
+                I agree to the
+                <span className="text-[#00BFA6]">&nbsp;Terms</span>
+                &nbsp;and
+                <span className="text-[#00BFA6]">&nbsp;Privacy Policy</span>.
+              </h3>
+            </div>
+            <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center">
+              <button className="btn btn-active btn-primary btn-block max-w-[200px]">
+                Sign Up
+              </button>
+              <button className="btn btn-outline btn-primary btn-block max-w-[200px]">
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
