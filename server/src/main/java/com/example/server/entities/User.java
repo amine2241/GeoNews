98b3ffb19d0609c09b9ab177e021602ecaedf787
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-04-30 13:47:56 +0100

    implemented User Entity

---
+++ server/src/main/java/com/example/server/entities/User.java
@@ -0,0 +1,25 @@
+package com.example.server.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String firstName;
+    private String  lastName;
+    private String  email;
+    private String password;
+}
