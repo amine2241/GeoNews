ce75f34865c3d6d90cbaac693ace1f8d2177ac68
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-05-01 00:46:55 +0100

    implemented addUser in service and web layer

--- server/src/main/java/com/example/server/service/UserService.java
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -1,4 +1,17 @@
 package com.example.server.service;
 
+import com.example.server.entities.User;
+import com.example.server.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepo userRepo;
+    public User addUser(User user) {
+        return userRepo.save(user);
+    }
+
+
 }
