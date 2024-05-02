92066ad0c73cb2c3f0c9db7b939cc31f13cfe787
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-05-02 01:42:04 +0100

    added thymleaf and validation dependency

--- server/src/main/java/com/example/server/entities/User.java
+++ server/src/main/java/com/example/server/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +20,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    @NotNull
+    @Size(min=2, max=30)
     private String firstName;
     private String  lastName;
     private String  email;
