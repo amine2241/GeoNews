8ea97b7dd770a9a38d00373648cd009d193ad6fe
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-05-08 16:32:04 +0100

    fix bound parameters and zoom

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -5,7 +5,7 @@ export default function Navbar(){
     function MapEvents() {
         const map = useMapEvents({
            click() {
-             console.log("hello")
+             console.log("hello");
              map.locate();
            },
            locationfound(e){
@@ -15,14 +15,16 @@ export default function Navbar(){
        
         return null; // This component doesn't render anything itself
        }
+
     
     return(
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} maxZoom={11} className="h-screen" >
+<MapContainer center={[51.505, -0.09]} maxBounds={[[-90, -180], [90, 180]]}  boundsOptions={{padding: [200, 200]}}stroke zoom={13} wrapAround = {true}  scrollWheelZoom={true} infinite = {true}maxZoom={11} minZoom={2} className="h-screen" >
   <TileLayer
     attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
     url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
     subdomains={'abcd'}
     maxZoom = { 20}
+   
   />
   <MapEvents/>
 
