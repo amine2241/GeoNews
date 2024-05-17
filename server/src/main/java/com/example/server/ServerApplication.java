62ce6dfd2188dfe40db01f43e3b9faaaac24bc6f
Said <t.said.acc@gmail.com>
2024-05-17 10:54:46 +0100

    removed project stuff

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -4,16 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import java.util.Locale;
 
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("lang/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        System.out.println(messageSource.getMessage("hello",null,Locale.FRENCH));
         SpringApplication.run(ServerApplication.class, args);
     }
 }
