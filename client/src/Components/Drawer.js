7f5bee36e44f1be5255ecd1a50f64f8887b4a64b
Said <t.said.acc@gmail.com>
2024-05-17 09:01:25 +0100

    oops news in spring cours

--- client/src/Components/Drawer.js
+++ client/src/Components/Drawer.js
@@ -1,37 +1,59 @@
 import {React, useEffect, useState} from 'react'
 import Navbar from '../Components/Navbar'
 import Map from '../Components/Map'
-import { preventDefault } from 'leaflet/src/dom/DomEvent'
+import News from '../Components/News'
 
 const Drawer = () => {
-  const [clicked, setClicked] = useState(false);
+
+
+    const [clicked, setClicked] = useState(true);
     const clickState =(state)=>{
         setClicked(state);
-    
       }
-  return (
-<div className="drawer drawer-end h-fit ">
-  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" checked ={clicked
-  } defaultChecked = {false} />
-  <div className="drawer-content">
-    {/* Page content here */}
-    <div className="sticky top-0 bottom-0 z-10">
-                <Navbar></Navbar>
-            </div>
-            <div className="sticky top-0 z-0">
-                <Map clickState={clickState}></Map>
-                
-            </div>
-           
-  </div> 
-  <div className="drawer-side top-16">
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
-</div>
+
+    const [Elcords, setElcords] = useState();
+    const cords = (cds) =>{
+        setElcords(cds)
+    }
+    const [showText, setshowText] = useState(false);
+    function getOut(){
+        setClicked(false)
+        setshowText(false)
+    }
+
+    function setHeightCheeseDiv() {
+        const height = document.getElementById('bar')?.clientHeight;
+        document.getElementById('cheese').style.height = `${height}px`;
+    }
+
+    useEffect(() => {
+        setHeightCheeseDiv();
+        setInterval(setHeightCheeseDiv, 1000);
+    }, []);
+
+
+
+    return (
+      <div className="drawer drawer-end h-fit ">
+
+          <input id="my-drawer-4" type="checkbox" className="drawer-toggle peer" checked={clicked} defaultChecked={false}/>
+          <div className="drawer-content">
+              {/* Page content here */}
+              <div className="sticky top-0 bottom-0 z-1">
+                  <Navbar></Navbar>
+              </div>
+              <div className="sticky" onClick={()=>setshowText(true)}>
+                  <Map clickState={clickState} cordsMap={cords}></Map>
+              </div>
+          </div>
+          <div  className="drawer-side top-16 z-2 " /*onClick={()=>setClicked(false)}*/>
+              <div id="cheese" className="mapdiv" onClick={()=>getOut()}></div>
+              <div id="bar">
+                  <News  cords={Elcords} clicked={clicked} showText={showText} ></News>
+              </div>
+          </div>
+
+      </div>
 
 
   )
